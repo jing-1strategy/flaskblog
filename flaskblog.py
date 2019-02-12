53bcf2d55194ecf3eaf0362c586b43099cf4bff1
Jing Liang <jing@1strategy.com>
2019-02-12 13:49:48 -0800

    Add error handling for 404 and 500 error.

--- flaskblog.py
+++ flaskblog.py
@@ -43,5 +43,14 @@ def home():
 def about():
     return render_template('about.html', title='About')
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', title="404 Not Found"), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template('500.html', title="500 Internal Error"), 500
+
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
